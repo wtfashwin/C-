76125dd9927c527d3e54be2b5ca76f028b3b6518
Ashwin Upadhyay <ashwinupadhyay09@gmail.com>
2024-09-10 18:41:55 +0530

    Push

---
+++ Functions basic.cpp
@@ -0,0 +1,17 @@
+/*Function 
+Create a function named “compute” which 
+takes two integer arguments A and B and returns an integer denoting A2 + B2. 
+*/
+
+#include <iostream>
+using namespace std;
+
+    int compute(int A, int B)
+
+{
+     return (A *A +  B * B);
+
+}
+
+return (A * A + B * B )
+
