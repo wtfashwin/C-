9d7fe951b3b3a7052762423bbb0a30f65a5c0552
Ashwin Upadhyay <ashwinupadhyay09@gmail.com>
2024-09-10 16:54:50 +0530

    Push

---
+++ Type Conversion.cpp
@@ -0,0 +1,20 @@
+/*Type Conversion
+
+You are given a character called ch, print the ASCII value of the character.
+Example Input:
+ch = 'a'
+Example Output:
+97
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()  
+{
+    char ch;
+    cin>>ch;
+    cout << (int)ch << endl;    
+    return 0;
+}
